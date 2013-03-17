7342f03fd19206ebe5da616b3a21a7cba09a1807
kebre001 <kebre001@gmail.com>
2013-03-17 16:48:08 +0100

    Explained more about the port

--- index.php
+++ index.php
@@ -1,9 +1,12 @@
 <html>
 	<head>
-		<?php exec("echo 1 > /dev/ttyACM0"); ?>
+		<?php
+		 exec("echo 1 > /dev/ttyACM0"); //This code can be different from computer to computer.
+		 								//ttyASCM0 is the port on the computer where Arduino is connected 
+		?>
 	</head>
 
 	<body>
-			<h4>Works</h4>
+		<h4>Works</h4>
 	</body>
 </html>
