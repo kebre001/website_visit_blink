fe5d4b014eb9f5d8214afb1dd6e5da2ad666e8a3
kebre001 <kebre001@gmail.com>
2013-03-17 16:44:50 +0100

    Arduino Code

---
+++ website_visit.ino
@@ -0,0 +1,23 @@
+	/* Arduino WebVisitors Blinker by Dimme.net */
+
+	int ledPin = 12;
+	int val = 0;
+
+	void setup() {
+	  Serial.begin(9600);
+	  pinMode(ledPin, OUTPUT);
+	}
+
+	void loop () {
+	  val = Serial.read(); 
+
+	  if (val > '0' && val <= '9') {
+	    val = val - '0';     
+	    for(int i=0; i<val; i++) {
+	      digitalWrite(ledPin,HIGH);
+	      delay(150);
+	      digitalWrite(ledPin, LOW);
+	      delay(150);
+	    }
+	  }
+	}
