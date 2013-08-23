7fbb2d7bb08b50c8ee717cf8227150c4750738e2
Kebre001 <kebre001@gmail.com>
2013-08-23 16:43:18 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 	<head>
 		<?php
 		 exec("echo 1 > /dev/ttyACM0"); //This code can be different from computer to computer.
-		 								//ttyASCM0 is the port on the computer where Arduino is connected 
+		 				//ttyASCM0 is the port on the computer where Arduino is connected 
 		?>
 	</head>
 
